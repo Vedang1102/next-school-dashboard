78eb7691c9d5f525c957ee0e476cf92672c7766e
Vedang1102 <atgurvedang@gmail.com>
2024-11-03 16:41:22 +0530

    Updated from Linux

--- src/app/components/CountChart.tsx
+++ src/app/components/CountChart.tsx
@@ -1,6 +1,65 @@
+"use client"
+import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
+import Image from 'next/image';
+
+const data = [
+  {
+    name: '18-24',
+    uv: 31.47,
+    pv: 2400,
+    fill: '#8884d8',
+  },
+  {
+    name: '25-29',
+    uv: 26.69,
+    pv: 4567,
+    fill: '#83a6ed',
+  },
+  {
+    name: '30-34',
+    uv: 15.69,
+    pv: 1398,
+    fill: '#8dd1e1',
+  },
+  {
+    name: '35-39',
+    uv: 8.22,
+    pv: 9800,
+    fill: '#82ca9d',
+  },
+  {
+    name: '40-49',
+    uv: 8.63,
+    pv: 3908,
+    fill: '#a4de6c',
+  },
+  {
+    name: '50+',
+    uv: 2.63,
+    pv: 4800,
+    fill: '#d0ed57',
+  },
+  {
+    name: 'unknow',
+    uv: 6.67,
+    pv: 4800,
+    fill: '#ffc658',
+  },
+];
+
 const CountChart = () => {
   return (
-    <div className=''>CountChart</div>
+    <div className='bg-white rounded-xl w-full h-full'>
+      {/* Title */}
+      <div className='flex justify-between items-center p-4'>
+        <h1>Students</h1>
+        <Image src='/moreDark.png' alt='' height={20} width={20}/>
+      </div>
+      {/* Chart */}
+      <div className=''></div>
+      {/* Bottom */}
+      <div className=''></div>
+    </div>
   )
 }
 
